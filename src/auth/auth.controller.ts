0dadad5fcf1818fd8522e62924de1135ba83767f
Rabin Mainali <rabinmainali1999@gmail.com>
2024-12-25 09:54:02 +0545

    passing dto as arg

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 
@@ -8,8 +9,8 @@ export class AuthController {
     
     }
     @Post('signup')
-    signup(){
-        return this.authService.signup()
+    signup(@Body() dto:AuthDto){
+        return this.authService.signup(dto)
     }
     
     @Post('signin')
