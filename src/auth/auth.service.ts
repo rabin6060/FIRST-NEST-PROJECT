43f741fa6ed9d452b93fcd2b3cf7873dbeadb0e8
Rabin Mainali <rabinmainali1999@gmail.com>
2024-12-25 10:57:49 +0545

    signin functionality completed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -14,17 +15,50 @@ export class AuthService {
         const hashPassword = await argon.hash(password)
 
         //create and save user to db
-        const user = await this.prisma.user.create({
-            data:{
-                email,
-                hash:hashPassword
+        
+        try {
+            const user = await this.prisma.user.create({
+                data:{
+                    email,
+                    hash:hashPassword
+                }
+            })
+            delete user.hash
+            //return user
+            return user
+        } catch (error) {
+            if(error instanceof PrismaClientKnownRequestError){
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Email already exixts')
+                }
             }
-        })
-        //return user
-        return user
+            throw error
+        }
+        
         
     }
-    signin(){
-        return {message: 'signin success'}
+    async signin(dto:AuthDto){
+        const {email,password} = dto
+        
+        try {
+            //find user by email
+            const user = await this.prisma.user.findUnique({
+                where:{
+                    email
+                }
+            })
+            if(!user){
+                throw new ForbiddenException("Invalid credentials")
+            }
+            //compare password
+            const isMatch = await argon.verify(user.hash,password)
+            if(!isMatch){
+                throw new ForbiddenException('Invalid credentials')
+            }
+            delete user.hash
+            return user
+        } catch (error) {
+            throw error.message
+        }
     }
 }
