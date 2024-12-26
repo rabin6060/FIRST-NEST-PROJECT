9a8dec1510b9d9d1ec18bd9384db7c08d0e76144
Rabin Mainali <rabinmainali1999@gmail.com>
2024-12-26 13:35:04 +0545

    function/services defined for crud operations

--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { BookmarkDto, EditBookmarkDto } from './dto';
 
 @Injectable()
-export class BookmarkService {}
+export class BookmarkService {
+
+    getBookmarks(userId:number) {}
+
+    getBookmark(userId:number,bookmarkId:number) {}
+
+    createBookmark(userId:number,dto:BookmarkDto) {}
+
+    editBookmark(userId:number,bookmarkId:number,dto:EditBookmarkDto) {}
+
+    deleteBookmark(userId:number,bookmarkId:number) {}
+}
