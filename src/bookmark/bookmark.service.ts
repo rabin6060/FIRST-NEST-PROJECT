44b48555b7dde7c34a28e3206c2ea4cb9ce84711
Rabin Mainali <rabinmainali1999@gmail.com>
2024-12-26 19:49:48 +0545

    edit and delete functionality added

--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,16 +1,88 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { BookmarkDto, EditBookmarkDto } from './dto';
+import { PrismaService } from '../../src/prisma/prisma.service';
 
 @Injectable()
 export class BookmarkService {
+    constructor(private prisma:PrismaService) {}
 
-    getBookmarks(userId:number) {}
+    getBookmarks(userId:number) {
+        return this.prisma.bookmark.findMany({
+            where:{
+                userId
+            }
+        })
+    }
 
-    getBookmark(userId:number,bookmarkId:number) {}
+    getBookmark(userId:number,bookmarkId:number) {
+        return this.prisma.bookmark.findUnique({
+            where:{
+                id:bookmarkId,
+                userId
+            }
+        })
+    }
 
-    createBookmark(userId:number,dto:BookmarkDto) {}
+    async createBookmark(userId:number,dto:BookmarkDto) {
+        try {
+            const bookmark =await this.prisma.bookmark.create({
+                data:{
+                    userId,
+                    ...dto
+    
+                    }    
+            })
+            return bookmark
+        } catch (error) {
+            throw new Error(error.message)
+            
+        }
+    }
 
-    editBookmark(userId:number,bookmarkId:number,dto:EditBookmarkDto) {}
+    async editBookmark(userId:number,bookmarkId:number,dto:EditBookmarkDto) {
+        try {
+            const bookmark =await this.prisma.bookmark.findUnique({
+                where:{
+                    id:bookmarkId
+                }
+            })
+            //check if bookmark userId is same as logged in user
+            if (!bookmark || bookmark.userId!==userId) {
+                 throw new ForbiddenException('unauthorized to edit bookmark')
+            }
+            //update the bookmark
+            const editedBookmark =await this.prisma.bookmark.update({
+                where:{
+                    id:bookmarkId,
+                },
+                data:{
+                    ...dto
+                }
+            })
+            return editedBookmark
+        } catch (error) {
+            throw new Error(error.message)
+        }
+    }
 
-    deleteBookmark(userId:number,bookmarkId:number) {}
+    async deleteBookmark(userId:number,bookmarkId:number) {
+        const bookmark =await this.prisma.bookmark.findUnique({
+            where:{
+                id:bookmarkId
+            }
+        })
+        //check if bookmark userId is same as logged in user
+            if (!bookmark || bookmark.userId!==userId) {
+                throw new ForbiddenException('unauthorized to delete bookmark')
+        }
+
+        //delete the bookmark
+       await this.prisma.bookmark.delete({
+        where:{
+            id:bookmarkId
+        }
+       })
+
+        
+    }
 }
